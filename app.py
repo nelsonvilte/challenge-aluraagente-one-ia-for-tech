07c7338719216e78ef7283cab6711e7b53d2075a
nelsonvilte <nelsonvilte@gmail.com>
2026-07-27 22:42:03 -0300

    actualizar modelo

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if archivo_cargado:
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     llm = ChatGroq(
         api_key=GROQ_API_KEY,
-        model_name="llama-3.1-8b-instant",
+        model_name="llama-3.3-70b-versatile",
         temperature=0
     )
 
--- herramientas.py
+++ herramientas.py
@@ -16,7 +16,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 llm = ChatGroq(
     api_key = GROQ_API_KEY,
-    model_name = 'llama3-70b-8192',
+    model_name = 'llama-3.3-70b-versatile',
     temperature = 0
 )
 
